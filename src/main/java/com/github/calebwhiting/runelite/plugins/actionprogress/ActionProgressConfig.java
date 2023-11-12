72215f64f7903436777854c89ece6cad2b601b98
Chris <christopherdk90@gmail.com>
2023-11-11 19:33:02 -0500

    change config name to what it should be

--- src/main/java/com/github/calebwhiting/runelite/plugins/actionprogress/ActionProgressConfig.java
+++ src/main/java/com/github/calebwhiting/runelite/plugins/actionprogress/ActionProgressConfig.java
@@ -52,7 +52,7 @@ public interface ActionProgressConfig extends Config
 			description = "Enable/Disable wintertodt specific actions.",
 			position = 14,
 			closedByDefault = true
-	) String WINTERTODT = "Wintertodt actions s";
+	) String WINTERTODT = "Wintertodt actions";
 	@ConfigSection(
 			name = "Miscellaneous actions",
 			description = "Enable/Disable miscellaneous actions.",
