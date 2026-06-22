e9291a909a3f2ed0b5d7aa4b4a9a2849d647e118
Krytlex <boudreauguillaume09@gmail.com>
2026-06-22 19:44:37 -0400

    Remove not null usage

--- src/main/java/com/github/calebwhiting/runelite/api/event/LocalAnimationChanged.java
+++ src/main/java/com/github/calebwhiting/runelite/api/event/LocalAnimationChanged.java
@@ -2,12 +2,11 @@ package com.github.calebwhiting.runelite.api.event;
 
 import lombok.Data;
 import net.runelite.api.Player;
-import org.jetbrains.annotations.NotNull;
 
 @Data
 public class LocalAnimationChanged
 {
 
-	@NotNull private final Player localPlayer;
+	private final Player localPlayer;
 
 }
